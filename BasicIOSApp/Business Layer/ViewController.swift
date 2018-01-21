880bc13cbe606eb2bd9c762ea6ac7ee3fadda89e
Andy Wu <hellowupeng@gmail.com>
2018-01-21 20:16:05 +0800

    添加Alamofire

---
+++ BasicIOSApp/Business Layer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicIOSApp
+//
+//  Created by Andy Wu on 1/21/18.
+//  Copyright © 2018 Andy Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '8.0'
+
+target 'BasicIOSApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for BasicIOSApp
+  pod 'Alamofire', '~> 4.6.0'
+
+end
